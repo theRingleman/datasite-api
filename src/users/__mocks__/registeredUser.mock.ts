d1f523a172b2c7cb288dac021f80902bcf890eb6
Sam Ringleman <sringleman@remax.com>
2021-03-18 15:49:54 -0600

    Add mocks for testing

---
+++ src/users/__mocks__/registeredUser.mock.ts
@@ -0,0 +1,17 @@
+const registeredUserMock = {
+  id: '1',
+  city: 'Jaydashire',
+  company: 'Goyette - Renner',
+  country: 'South Africa',
+  firstName: 'firstName 1',
+  lastName: 'lastName 1',
+  organizationType: 'organizationType 1',
+  phone: '524.276.1570 x487',
+  state: 'SD',
+  zipCode: '68048',
+  disclaimerAccepted: false,
+  languageCode: 'en',
+  emailAddress: 'last1@mail.com',
+};
+
+export default registeredUserMock;
---
+++ src/users/__mocks__/unregisteredUser.mock.ts
@@ -0,0 +1,10 @@
+import { UnregisteredUserInterface } from '../interfaces/unregisteredUser.interface';
+
+const unregisteredUsers: UnregisteredUserInterface = {
+  id:"21",
+  emailAddress:"email1@somewhere.com",
+  languageCode:"en", registrationId:"jwsMJNOk3oM3hVM5bGcF1",
+  registrationIdGeneratedTime:"156165026851"
+}
+
+export default unregisteredUsers;
